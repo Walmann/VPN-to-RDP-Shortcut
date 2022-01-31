26f0fe92311322fbf8b74b0c431bfb8be36d669c
Tov Are <tovare13@gmail.com>
2022-01-31 14:35:10 +0100

    It now import correctly

--- TEMPLATE.ps1
+++ TEMPLATE.ps1
@@ -2,11 +2,11 @@ $Arguments = @{
     
     RDP_Address     = "remote.example.no"
     RDP_Username    = "Username"
-    RDP_Port        = ""                    #Viss denne er tom brukes standard port
+    # RDP_Port        = ""                    #Viss denne er tom brukes standard port
     RDP_Server_IP   = "10.10.10.10"         #Må være IP addresse
     VPN_Name        = "TempVpn"             #Navnet til VPN tilkoblingen i .dep\Phonebook.pbk
-    VPN_User        = ""                    #Viss denne er tom brukes RDP_Username
+    # VPN_User        = ""                    #Viss denne er tom brukes RDP_Username
 }
-
+    #OBS!!! Argumenter som ikke er i bruk må kommenteres ut med "#"
 
 Powershell.exe -executionpolicy Bypass -Command  ".\dep\_MainConnect.ps1" @Arguments
